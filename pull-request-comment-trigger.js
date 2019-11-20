4195aa58ec5ee04d07d72597045ec66e8bb0f935
Jared Forsyth <jared@jaredforsyth.com>
2019-11-20 16:19:45 -0700

    ok ready

--- pull-request-comment-trigger.js
+++ pull-request-comment-trigger.js
@@ -1,33 +1,6 @@
 const core = require('@actions/core');
 const {context, GitHub} = require('@actions/github')
 
-// send a thumbs up to the comment
-const thumbsUp = ({owner, repo, id}) => {
-    const client = new GitHub(process.env.GITHUB_TOKEN);
-
-    return client.reactions.createForIssueComment({
-        owner,
-        repo,
-        comment_id: id,
-        content: 'rocket'
-    })
-}
-
-const getData = () => {
-    if (context.eventName === 'issue_comment') {
-        return {
-            body: context.payload.comment.body,
-            id: context.payload.comment.id,
-        }
-    } else {
-        return {
-            body: context.payload.pull_request.body,
-            id: context.payload.pull_request.id,
-        }
-    }
-}
-
-// most @actions toolkit packages have async methods
 async function run() {
     const trigger = core.getInput('trigger');
     if (!trigger) {
@@ -35,20 +8,34 @@ async function run() {
         return
     }
 
-    console.log(context.eventName)
-    console.log(context)
-    console.log(context.payload)
-    core.debug((new Date()).toTimeString())
-    core.debug((new Date()).toTimeString())
     const {owner, repo} = context.repo;
 
-    const {body, id} = getData();
-    if (body.includes(trigger)) {
-        core.setOutput('triggered', 'true');
-        console.log('sending a thumbs up to', owner, repo, id)
-        await thumbsUp({owner, repo, id})
-    } else {
+    const body = context.eventName === 'issue_comment'
+        ? context.payload.comment.body
+        : context.payload.pull_request.body;
+
+    if (!body.includes(trigger)) {
         core.setOutput('triggered', 'false');
+        return;
+    }
+
+    core.setOutput('triggered', 'true');
+
+    const client = new GitHub(process.env.GITHUB_TOKEN);
+    if (context.eventName === 'issue_comment') {
+        await client.reactions.createForIssueComment({
+            owner,
+            repo,
+            comment_id: context.payload.comment.id,
+            content: 'rocket'
+        })
+    } else {
+        await octokit.reactions.createForIssue({
+            owner,
+            repo,
+            issue_number: context.payload.pull_request.number,
+            content: 'rocket'
+        })
     }
 }
 
